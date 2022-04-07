f7b67312f22883d81251372b1c1670065c49b735
Mario <mcampohe@gmail.com>
2022-04-07 15:10:26 -0400

    Arreglo en vista

--- src/app/components/componente-b/componente-b.component.ts
+++ src/app/components/componente-b/componente-b.component.ts
@@ -5,19 +5,23 @@ import { ObjetosService } from 'src/app/service/objetos.service';
   selector: 'app-componente-b',
   templateUrl: './componente-b.component.html',
   styleUrls: ['./componente-b.component.css']
+
 })
 export class ComponenteBComponent implements OnInit {
 
   obj = [{}];
-
+ 
   constructor(private service: ObjetosService) { }
-
+  
   ngOnInit(): void {
-    this.obj = this.service.getData();
-    
-    console.log(this.obj);
+
+    for (let i=0; i < this.service.getData().length; i++){
+      this.obj.push(this.service.getData()[i].nombre +'   -   '+this.service.getData()[i].email); 
+      //console.log(this.service.getData()[i].nombre);
+    }
 
   }
+ 
   
 
 }
